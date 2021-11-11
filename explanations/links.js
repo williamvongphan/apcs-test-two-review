a82335ec681149e63f09b6f449e8c8b70706d569
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-11 22:41:37 +0000

    links, attempt 2

--- explanations/links.js
+++ explanations/links.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 
 for (x = 1; x < 10; x++) {
-	fs.appendFileSync("./readme.md", `[Question ${x}](https://thunderredstar.me/Test-2-Review/explanations/the_part_with_multiple_guesses/1-9/${x})`)
+	fs.appendFileSync("./readme.md", `${x}. [Question ${x}](https://thunderredstar.me/Test-2-Review/explanations/the_part_with_multiple_guesses/1-9/${x})`)
 }
