96722f4f24260fe85a8ebf2720163f17719148f1
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-11 20:43:15 +0000

    add demo, save work

---
+++ explanations/the_part_with_multiple_guesses/1-9/2/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+	public static void main (String[] args) {
+		System.out.print("Choice a) prints ");
+		System.out.println(9 + 0.95);
+
+		System.out.print("Choice b) prints ");
+		System.out.println(995/100.0);
+
+		System.out.print("Choice c) prints ");
+		System.out.println(9. + 95/100);
+
+		System.out.print("Choice d) prints ");
+		System.out.println(9 + 95.0/100);
+
+		System.out.print("Choice e) prints ");
+		System.out.println(9 + "." + 95);
+	}
+}
