2ae033f56385b281277aba70fd3de6ce06c6905f
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-11 22:40:54 +0000

    links

--- explanations/links.js
+++ explanations/links.js
@@ -1,3 +1,5 @@
 const fs = require("fs");
 
-let files = fs.readdirSync("./the_part_with_")
+for (x = 1; x < 10; x++) {
+	fs.appendFileSync("./readme.md", `[Question ${x}](https://thunderredstar.me/Test-2-Review/explanations/the_part_with_multiple_guesses/1-9/${x})`)
+}
