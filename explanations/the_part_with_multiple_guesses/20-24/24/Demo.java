bd541aa84621a76522d6a58603a7dda3a7407d84
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 01:58:22 +0000

    turns out the part with multiple responses didn't matter

---
+++ explanations/the_part_with_multiple_guesses/20-24/24/Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+	public static void main (String[] args) {
+		int n = 253;
+		for (int i = 0; i < 50; x++) n = (n + 3) / 2
+	}
+}
---
+++ explanations/the_part_with_multiple_guesses/20-24/24/readme.md
@@ -0,0 +1,4 @@
+# Question 24
+## Question
+What is the value of n after the following code is executed?
+```java```
