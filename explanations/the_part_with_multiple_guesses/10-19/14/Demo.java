1f510262bd414a7c3696bddfb2a5cc6528b1c706
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 04:17:35 +0000

    repair broken demo

--- explanations/the_part_with_multiple_guesses/10-19/14/Demo.java
+++ explanations/the_part_with_multiple_guesses/10-19/14/Demo.java
@@ -1,4 +1,4 @@
-public static Demo {
+public class Demo {
 	public static void main (String[] args) {
 		System.out.println("Answer choice a evaluates to " + (double)(25/4))
 		System.out.println("Answer choice b evaluates to " + 25/(double)4.0)
