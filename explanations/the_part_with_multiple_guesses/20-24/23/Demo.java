d06d16702216f9fe8f84542ef84b2084652a9707
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 01:49:39 +0000

    demo

--- explanations/the_part_with_multiple_guesses/20-24/23/Demo.java
+++ explanations/the_part_with_multiple_guesses/20-24/23/Demo.java
@@ -1,9 +1,12 @@
 public class Demo {
 	public static void twist (String[] w) {
 		String temp = w[0].substring(0, 1);
-		w[0] = w[1].substring()
+		w[0] = w[1].substring(0, 1) + w[0].substring(1);
+		w[1] = temp + w[1].substring(1);
 	}
 	public static void main (String[] args) {
 		String[] words = {"HOW", "NEAT"};
+		twist(words);
+		System.out.println(words[0] + " " + words[1]);
 	}
 }
