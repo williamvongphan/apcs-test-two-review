c71e7c501bc7525de3e989187f8a7781fd8d072d
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 02:02:47 +0000

    almost thERE!!!

--- explanations/the_part_with_multiple_guesses/20-24/24/Demo.java
+++ explanations/the_part_with_multiple_guesses/20-24/24/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
 	public static void main (String[] args) {
 		int n = 253;
 		for (int i = 0; i < 50; i++) n = (n + 3) / 2;
+		System.out.println("n is now " + n);
 	}
 }
--- explanations/the_part_with_multiple_guesses/20-24/24/readme.md
+++ explanations/the_part_with_multiple_guesses/20-24/24/readme.md
@@ -2,4 +2,9 @@
 ## Question
 What is the value of n after the following code is executed?
 ```java
+int n = 253;
+for (int i = 0; i < 50; i++) n = (n + 3) / 2;
 ```
+a) 1
+b) 2
+c)
