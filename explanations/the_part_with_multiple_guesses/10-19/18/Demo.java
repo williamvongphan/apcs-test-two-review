1e9d310e3aedbdc894bb949e1953e9d6a810951d
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:51:05 +0000

    demoscene radio

---
+++ explanations/the_part_with_multiple_guesses/10-19/18/Demo.java
@@ -0,0 +1,11 @@
+/* Are there errors during compile? */
+
+public class Demo {
+	public static void main(String[] args) {
+		int x = 160;
+		byte x1 = 160;
+		short x2 = 160;
+		long x3 = 160;
+		double x4 = 160;
+	}
+}
