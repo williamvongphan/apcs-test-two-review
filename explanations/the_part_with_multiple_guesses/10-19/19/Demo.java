987aa4491b203f2ecc04d11822df5cb3b4c900ed
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:59:59 +0000

    demoscene radio

--- explanations/the_part_with_multiple_guesses/10-19/19/Demo.java
+++ explanations/the_part_with_multiple_guesses/10-19/19/Demo.java
@@ -1,3 +1,10 @@
 public class Demo {
-	public static void main ()
+	public static void main (String[] args) {
+		int x = 123; int y = 0;
+		while (x > 0) {
+			y *= 10;
+			y += x % 10;
+			x /= 10
+		}
+	}
 }
