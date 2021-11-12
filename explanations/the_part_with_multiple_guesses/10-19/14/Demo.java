cedc104e634a83686bc3a63904e54540a0f82e10
William <67127399+thunder-red-star@users.noreply.github.com>
2021-11-12 00:13:00 +0000

    demo14

---
+++ explanations/the_part_with_multiple_guesses/10-19/14/Demo.java
@@ -0,0 +1,9 @@
+public static Demo {
+	public static void main (String[] args) {
+		System.out.println("Answer choice a evaluates to " + (double)(25/4))
+		System.out.println("Answer choice b evaluates to " + 25/(double)4.0)
+		System.out.println("Answer choice c evaluates to " + (double)25/4)
+		System.out.println("Answer choice d evaluates to " + 25/4.0)
+		System.out.println("Answer choice e evaluates to " + 25.0/4)
+	} 
+}
